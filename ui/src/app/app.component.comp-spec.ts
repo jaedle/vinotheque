d21e8384648d414bf40c140324781f6262848fb0
Dennis Jekubczyk <dennis.jekubczyk@gmail.com>
2020-10-16 15:49:56 +0200

    add ui component tests

---
+++ ui/src/app/app.component.comp-spec.ts
@@ -0,0 +1,9 @@
+import {Selector} from 'testcafe';
+
+fixture`Vinotheque`
+  .page`http://localhost:4200/`;
+
+test('Title is correct', async t => {
+  await t
+    .expect(Selector("title").innerText).eql('Vinotheque');
+});
