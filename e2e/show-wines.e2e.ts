bc0e668a13105f05205505141eaaea5bce2ee09c
Dennis Jekubczyk <dennis.jekubczyk@gmail.com>
2020-10-16 16:19:30 +0200

    add first component test

--- e2e/show-wines.e2e.ts
+++ e2e/show-wines.e2e.ts
@@ -3,14 +3,7 @@ import {Selector} from 'testcafe';
 fixture`Vinotheque`
     .page`http://localhost:8000/`;
 
-test('Title is correct', async t => {
-    await t
-        .expect(Selector("title").innerText).eql('Vinotheque');
-});
-
-test('shows wines', async t => {
+test('ui fetches wines from backend', async t => {
     await t
         .expect(Selector('.wine-name').withText('Great Shiraz').visible).eql(true);
-    await t
-        .expect(Selector('.wine-name').withText('Wodden Pinot Noir').visible).eql(true);
 });
--- ui/src/app/app.component.comp-spec.ts
+++ ui/src/app/app.component.comp-spec.ts
@@ -1,9 +1,29 @@
 import {Selector} from 'testcafe';
 
-fixture`Vinotheque`
-  .page`http://localhost:4200/`;
+import {RequestMock} from 'testcafe';
 
-test('Title is correct', async t => {
+const mock = RequestMock()
+  .onRequestTo('http://localhost:4200/api/wines')
+  .respond({
+      "wines": [
+        {"name": "Great Shiraz"},
+        {"name": "Wodden Pinot Noir"}
+      ]
+    }
+  )
+
+fixture`Vinotheque`
+  .page`http://localhost:4200/`
+  .requestHooks(mock);
+
+test('title is correct', async t => {
   await t
     .expect(Selector("title").innerText).eql('Vinotheque');
+});
+
+test('shows wines', async t => {
+  await t
+    .expect(Selector('.wine-name').withText('Great Shiraz').visible).eql(true);
+  await t
+    .expect(Selector('.wine-name').withText('Wodden Pinot Noir').visible).eql(true);
 });
