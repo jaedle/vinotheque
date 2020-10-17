ccb4c95d62618995b1388b6d0efdcc46db7bc934
Dennis Jekubczyk <dennis.jekubczyk@gmail.com>
2020-10-17 10:32:34 +0200

    add source formating for e2e

--- e2e/show-wines.e2e.ts
+++ e2e/show-wines.e2e.ts
@@ -1,9 +1,9 @@
-import {Selector} from 'testcafe';
+import { Selector } from 'testcafe';
 
-fixture`Vinotheque`
-    .page`http://localhost:8000/`;
+fixture`Vinotheque`.page`http://localhost:8000/`;
 
-test('ui fetches wines from backend', async t => {
-    await t
-        .expect(Selector('.wine-name').withText('Great Shiraz').visible).eql(true);
+test('ui fetches wines from backend', async (t) => {
+  await t
+    .expect(Selector('.wine-name').withText('Great Shiraz').visible)
+    .eql(true);
 });
