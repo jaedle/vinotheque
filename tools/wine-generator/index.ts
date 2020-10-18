65d8f783b4c920b40d33ae270e85a7e9186cc749
Dennis Jekubczyk <dennis.jekubczyk@gmail.com>
2020-10-18 19:59:13 +0200

    add dummy dev data

--- tools/wine-generator/index.ts
+++ tools/wine-generator/index.ts
@@ -1,1 +1,26 @@
-console.log('hello world');
+import * as YAML from 'yaml'
+import * as faker from 'faker';
+
+const types = ['red', 'rose', 'white', 'sparkling'];
+
+function aType() {
+    return types[Math.floor(Math.random() * types.length)];
+}
+
+function aWine() {
+    return {
+        name: faker.commerce.productName(),
+        winery: faker.company.companyName(),
+        type: aType(),
+    };
+}
+
+const wines = [];
+for (let i = 0; i < 200; i++) {
+    wines.push(aWine());
+}
+const result = {wines: wines};
+console.log(YAML.stringify(result));
+
+
+
