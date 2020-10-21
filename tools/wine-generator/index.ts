226e2a3d0858a5aa9f9eab9bd32f432cff9e7809
Dennis Jekubczyk <dennis.jekubczyk@gmail.com>
2020-10-21 18:33:00 +0200

    use more realistic sample wine names

--- tools/wine-generator/index.ts
+++ tools/wine-generator/index.ts
@@ -3,16 +3,27 @@ import * as faker from 'faker';
 
 const types = ['red', 'rose', 'white', 'sparkling'];
 
+const grapes = {
+    red: ['Bordeaux', 'Burgundy', 'Cabernet Sauvignon', 'Chianti', 'Malbec', 'Rioja', 'Shiraz', 'Zinfandel', 'Pinot Noir'],
+    rose: ['Burgundy', 'Merlot', 'Malbec', 'Zinfandel', 'Pinot Noir'],
+    white: ['Chardonnay', 'Pinot Gris', 'Riesling', 'Zinfandel', 'Sauvignon Blanc'],
+    sparkling: ['Pinot Gris', 'Riesling', 'Pinot Noir'],
+}
+
 function aType() {
     return types[Math.floor(Math.random() * types.length)];
 }
 
 function aWine() {
+    const type = aType();
+
+    const possibleGrapes = grapes[type];
+    const grape = possibleGrapes[Math.floor(Math.random() * possibleGrapes.length)];
     return {
         name: faker.commerce.productName(),
         winery: faker.company.companyName(),
-        type: aType(),
-        grape: faker.commerce.productName(),
+        type: type,
+        grape: grape,
         year: faker.date.past(20).getFullYear()
     };
 }
