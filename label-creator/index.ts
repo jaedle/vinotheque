ebe5ad4ac23b9c5b3b8692dc189393cab443d27c
Dennis Jekubczyk <dennis.jekubczyk@gmail.com>
2020-10-24 09:56:18 +0200

    add label creator

---
+++ label-creator/index.ts
@@ -0,0 +1,81 @@
+import {jsPDF,} from "jspdf";
+import * as QRCode from 'qrcode'
+
+const baseUrl = `https://vinotheque.jaedle.de`;
+const output = "output/labels.pdf";
+
+const labels = {
+    width: 40,
+    height: 40,
+    leftBorder: 16,
+    topBorder: 13.5,
+    perLine: 4,
+    rowsPerPage: 6,
+    verticalDistance: 6,
+    horizontalDistance: 6,
+}
+
+
+async function QrCodeFor(bottle: number) {
+    return await QRCode.toBuffer(
+        `${baseUrl}/link?bottle=${bottle}`,
+        {
+            type: "png",
+            errorCorrectionLevel: "high",
+            margin: 0,
+        }
+    );
+}
+
+const codeWidthHeight = (labels.width / 2) * Math.sqrt(2) * 0.9;
+
+
+async function main() {
+    const doc = new jsPDF({
+        format: 'A4'
+    });
+
+    let bottle = 10000;
+
+    async function addLabel(startX: number, startY: number) {
+        const centerX = startX + labels.width / 2;
+        const centerY = startY + labels.height / 2;
+        // doc.circle(
+        //     centerX,
+        //     centerY,
+        //     labels.height / 2,
+        //     'S'
+        // );
+        
+        doc.addImage(await QrCodeFor(bottle), "png",
+            centerX - codeWidthHeight / 2,
+            centerY - codeWidthHeight / 2,
+            codeWidthHeight,
+            codeWidthHeight);
+        doc.text(`${bottle}`,
+            centerX,
+            centerY - codeWidthHeight / 2,
+            {align: "center", baseline: "bottom"}
+        );
+    }
+
+    for (let row = 0; row < labels.rowsPerPage; row++) {
+        const startY = labels.topBorder
+            + row * labels.verticalDistance
+            + row * labels.height;
+        for (let labelInLine = 0; labelInLine < labels.perLine; labelInLine++) {
+            const startX = labels.leftBorder
+                + labels.width * labelInLine
+                + labels.horizontalDistance * labelInLine;
+            await addLabel(startX, startY);
+
+            bottle++;
+        }
+    }
+
+
+    doc.save(output);
+
+}
+
+main();
