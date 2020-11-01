7890e7b444580e9097c5ad207b39a303e2775799
Dennis Jekubczyk <dennis.jekubczyk@gmail.com>
2020-11-01 11:45:41 +0100

    use local https

--- e2e/show-wines.e2e.ts
+++ e2e/show-wines.e2e.ts
@@ -1,6 +1,6 @@
 import {Selector} from 'testcafe';
 
-fixture`Vinotheque`.page`http://localhost:8000/`;
+fixture`Vinotheque`.page`https://vinotheque.foo.bar:8443/`;
 
 test('shows wine overview', async (t) => {
   await t
@@ -14,7 +14,7 @@ test('shows wine details', async (t) => {
   await t.expect(Selector('body').withText('RED').visible).eql(true);
 });
 
-fixture`External links`.page`http://localhost:8000/link?bottle=1337`;
+fixture`External links`.page`https://vinotheque.foo.bar:8443/link?bottle=1337`;
 
 test('opens wine details by bottle id', async (t) => {
   await t.expect(Selector('body').withText('RED').visible).eql(true);
--- reverse-proxy/wait-healthy.sh
+++ reverse-proxy/wait-healthy.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 
 RETRIES=500
 current=0
-until curl -fsSL localhost:8000/health &> /dev/null; do
+until curl -fsSL https://vinotheque.foo.bar:8443/health &> /dev/null; do
   current=$((current+1))
   if [[ $current -gt $RETRIES ]]; then
     echo 'reverse-proxy did not get healthy; failing'
