bdb98c70d53a6258d0c66481cf334294186159d7
Dennis Jekubczyk <dennis.jekubczyk@gmail.com>
2020-11-01 10:45:24 +0100

    replace self built reverse proxy with caddy

--- reverse-proxy/wait-healthy.sh
+++ reverse-proxy/wait-healthy.sh
@@ -7,7 +7,7 @@ current=0
 until curl -fsSL localhost:8000/health &> /dev/null; do
   current=$((current+1))
   if [[ $current -gt $RETRIES ]]; then
-    &>2 echo 'reverse-proxy did not get healthy; failing'
+    echo 'reverse-proxy did not get healthy; failing'
     exit 1
   fi
   sleep 0.1
--- ui/wait-healthy.sh
+++ ui/wait-healthy.sh
@@ -7,7 +7,7 @@ current=0
 until curl -fsSL localhost:4200/ &> /dev/null; do
   current=$((current+1))
   if [[ $current -gt $RETRIES ]]; then
-    &>2 echo 'ui did not get healthy; failing'
+    echo 'ui did not get healthy; failing'
     exit 1
   fi
   sleep 0.1
