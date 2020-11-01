0004e58336e22c64f0603a39ecc17f3045f8c71f
Dennis Jekubczyk <dennis.jekubczyk@gmail.com>
2020-11-01 11:52:10 +0100

    add verbosity

--- reverse-proxy/wait-healthy.sh
+++ reverse-proxy/wait-healthy.sh
@@ -8,6 +8,7 @@ until curl -fsSL https://vinotheque.foo.bar:8443/health &> /dev/null; do
   current=$((current+1))
   if [[ $current -gt $RETRIES ]]; then
     echo 'reverse-proxy did not get healthy; failing'
+    curl -v https://vinotheque.foo.bar:8443/health -D -
     exit 1
   fi
   sleep 0.1
