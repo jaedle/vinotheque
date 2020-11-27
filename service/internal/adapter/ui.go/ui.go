e00c84b1ba7a05aa1836a20dc96c5d00aeedd69c
Dennis Jekubczyk <dennis.jekubczyk@gmail.com>
2020-11-27 13:52:26 +0100

    send wines through api

--- service/internal/adapter/ui.go/ui.go
+++ service/internal/adapter/ui.go/ui.go
@@ -2,21 +2,25 @@ package ui
 
 import (
 	"encoding/json"
+	"github.com/jaedle/vinotheque/service/internal/domain"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
-func NewUi(r *httprouter.Router) {
-	r.GET("/api/v2/wines", listWines())
+type context struct {
+	repo domain.WineRepository
 }
 
-func listWines() func(w http.ResponseWriter, rq *http.Request, _ httprouter.Params) {
+func NewUi(r *httprouter.Router, repo domain.WineRepository) {
+	c := context{repo: repo}
+	r.GET("/api/v2/wines", c.listWines())
+}
+
+func (c *context) listWines() func(w http.ResponseWriter, rq *http.Request, _ httprouter.Params) {
 	return func(w http.ResponseWriter, rq *http.Request, _ httprouter.Params) {
+		wines, _ := c.repo.LoadAll()
 		w.Header().Add("Content-Type", "application/json")
-		result := listWinesResponse{
-			Wines: []wineDto{},
-		}
-		body, _ := json.Marshal(result)
+		body, _ := json.Marshal(toListWineResponse(wines))
 		_, _ = w.Write(body)
 		w.WriteHeader(http.StatusOK)
 	}
@@ -27,4 +31,22 @@ type listWinesResponse struct {
 }
 
 type wineDto struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func toListWineResponse(wines []*domain.Wine) *listWinesResponse {
+	var result []wineDto
+	for _, w := range wines {
+		result = append(result, toWineDto(w))
+	}
+
+	return &listWinesResponse{Wines: result}
+}
+
+func toWineDto(wine *domain.Wine) wineDto {
+	return wineDto{
+		Id:   wine.GetId().Value(),
+		Name: wine.GetName().Value(),
+	}
 }
--- service/internal/adapter/ui.go/ui_test.go
+++ service/internal/adapter/ui.go/ui_test.go
@@ -18,7 +18,7 @@ var _ = Describe("Ui", func() {
 	BeforeSuite(func() {
 		repo = testconfig.WineRepository()
 		router = httprouter.New()
-		ui.NewUi(router)
+		ui.NewUi(router, repo)
 	})
 
 	BeforeEach(func() {
@@ -27,11 +27,25 @@ var _ = Describe("Ui", func() {
 
 	Context("GET /api/v2/wines", func() {
 		It("returns wines", func() {
+			id1 := domain.WineIdOf("a-wine-id")
+			name1 := domain.WineNameOf("a-wine-name")
+			wine1 := domain.NewWine(id1, name1)
+			_ = repo.Save(wine1)
+			id2 := domain.WineIdOf("another-wine-id")
+			name2 := domain.WineNameOf("another-wine-name")
+			wine2 := domain.NewWine(id2, name2)
+			_ = repo.Save(wine2)
+
 			res := get(router, "/api/v2/wines")
 
 			Expect(res.Code).To(Equal(http.StatusOK))
 			Expect(res.Header().Get("Content-Type")).To(Equal("application/json"))
-			Expect(res.Body.String()).To(Equal(`{"wines":[]}`))
+			Expect(res.Body.String()).To(MatchJSON(`{
+			"wines":[
+				{"id": "a-wine-id", "name": "a-wine-name"},
+				{"id": "another-wine-id", "name": "another-wine-name"}
+			]}`),
+			)
 		})
 	})
 
